10366c3a6bbdc54a23491cce1bb8629b47b94443
Guccifer808 <36012294+Guccifer808@users.noreply.github.com>
2022-12-27 21:49:55 +0200

    Updated

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 //creating reusable axios URL
 //admin token
 const BASE_URL = 'http://localhost:5000/api';
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTg0ZmNkMWI3NmM3NjgxZWY2YzAwMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MjA2OTczOSwiZXhwIjoxNjcyMTU2MTM5fQ.8kW6QSc1UcGjQgCxq8y7_d2VOoqXHriCpKAp8x0lRxg';
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
